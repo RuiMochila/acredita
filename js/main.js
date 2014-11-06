66a516cd52eea9007b9adbbbd95d25bc4a399708
Rui Mochila <rui.mochila@gmail.com>
2014-11-06 12:01:41 +0000

    Mudei o action do form

--- js/main.js
+++ js/main.js
@@ -8,39 +8,42 @@ $( document ).ready(function(){
 	  // this.hide();
 	});
 
-	// var form = $('form');
-	// $("form").on("submit",function( event ) {
- //  		event.preventDefault();
- //  		console.log("not");
- //  		// $.ajax({
- //    //         type: form.attr('method'),
- //    //         url: form.attr('action'),
- //    //         data: form.serialize(),
- //    //         dataType: 'json',
- //    //         success: function (data) {
- //    //         	console.log(data);
- //    //         },
- //    //         error: function(err){
- //    //         	console.log(err);	
- //    //         }
- //    //     });
- //    $.ajax({
-	// url: 'https://www51.e-goi.com/api/public/commons/ping',
-	// contentType: 'application/json',
-	// type: 'POST',
-	// dataType: 'json',
-	// data: JSON.stringify({
-	// 	foo: "This text is left of",
-	// 	bar: "this text..."
-	// }),
-	// success: function(data) {
-	// 	var output = data.responseText;
-	// 	console.log(output);
-	// 	// ... do something useful
-	// }
-	// });
- //  		// Submit ajax form and treat answer.
-	// });
+	var form = $('form');
+	$("form").on("submit",function( event ) {
+  		
+  		// event.preventDefault();
+  		// console.log("not");
+  		// $.ajax({
+    //         type: form.attr('method'),
+    //         url: form.attr('action'),
+    //         data: form.serialize(),
+    //         dataType: 'json',
+    //         success: function (data) {
+    //         	console.log(data);
+    //         },
+    //         error: function(err){
+    //         	console.log(err);	
+    //         }
+    //     });
+
+
+		 //    $.ajax({
+			// url: 'https://www51.e-goi.com/api/public/commons/ping',
+			// contentType: 'application/json',
+			// type: 'POST',
+			// dataType: 'json',
+			// data: JSON.stringify({
+			// 	foo: "This text is left of",
+			// 	bar: "this text..."
+			// }),
+			// success: function(data) {
+			// 	var output = data.responseText;
+			// 	console.log(output);
+			// 	// ... do something useful
+			// }
+			// });
+		 //  		// Submit ajax form and treat answer.
+	});
 
 
 });
